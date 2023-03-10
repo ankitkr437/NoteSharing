48e23e63c65fe67fde257a1eaa859631b7be470a
ankitkr437 <ankitloharshi@gmail.com>
2023-03-11 00:01:44 +0530

    made better

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -5,9 +5,9 @@ const BASE_URL = "https://notesharingbackend-ankitkr437.onrender.com/api/";
 export const pf="https://notesharingbackend-ankitkr437.onrender.com/images";
 
 export const publicRequest = axios.create({
-  baseURL: LocalBASE_URL,
+  baseURL: BASE_URL,
 });
 
 export const userRequest = axios.create({
-  baseURL: LocalBASE_URL,
+  baseURL: BASE_URL,
 });
