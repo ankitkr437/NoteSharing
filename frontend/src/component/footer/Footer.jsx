25bb86ba1613b8a3e1ced3d938d37cd6f403575a
ANKIT KUMAR <91136267+ankitkr437@users.noreply.github.com>
2024-07-14 23:08:10 +0530

    Update Footer.jsx

--- frontend/src/component/footer/Footer.jsx
+++ frontend/src/component/footer/Footer.jsx
@@ -23,8 +23,8 @@ const Footer = () => {
           </div>
 
           <div className="Contact-contribute-container">
-              <a href="mailto:abuzareqbal2600@gmail.com" className="about-us-title" target="blank">Contact Us</a>   
-              <a href="https://github.com/theAbuzar26/NoteSpreader" className="about-us-title" target="blank">Want to contribute</a>   
+              <a href="mailto:ankitloharshi@gmail.com" className="about-us-title" target="blank">Contact Us</a>   
+              <a href="https://github.com/ankitkr437/NoteSpreader" className="about-us-title" target="blank">Want to contribute</a>   
           </div>
           </div>
 
